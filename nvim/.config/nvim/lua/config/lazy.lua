e9a1c86e5fcc92404f8def13df7d95f91a82956c
Kevin Ruiz <niveksoft@proton.me>
2024-05-01 18:49:55 -0600

    disable netrw plugins

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -5,6 +5,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
+
+---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup {
@@ -36,7 +38,10 @@ require("lazy").setup {
                 "gzip",
                 -- "matchit",
                 -- "matchparen",
-                -- "netrwPlugin",
+                "netrw",
+                "netrwPlugin",
+                "netrwSettings",
+                "netrwFileHandlers",
                 "tarPlugin",
                 "tohtml",
                 "tutor",
