871e720262da44a99f2f4df341fe347aaecdf300
nikevsoft <26445418+nikevsoft@users.noreply.github.com>
2024-06-10 21:29:02 -0600

    use extras instead of custom plugins

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "MattiasMTS/cmp-dbee", ft = "sql" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-        table.insert(opts.sources, { name = "cmp-dbee" })
-    end,
-}
--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -2,6 +2,4 @@ return {
     { "nvim-neo-tree/neo-tree.nvim", enabled = false },
     { "akinsho/bufferline.nvim", enabled = false },
     { "rcarriga/nvim-notify", enabled = false },
-    { "lukas-reineke/indent-blankline.nvim", enabled = false },
-    { "echasnovski/mini.indentscope", enabled = false },
 }
