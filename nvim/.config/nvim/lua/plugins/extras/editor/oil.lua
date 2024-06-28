763b07055635b66740250dcd6b2d0b3b6fce9182
nikevsoft <26445418+nikevsoft@users.noreply.github.com>
2024-06-28 15:16:51 -0600

    move oil config to extras

---
+++ nvim/.config/nvim/lua/plugins/extras/editor/oil.lua
@@ -0,0 +1,49 @@
+return {
+    { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+    {
+        "stevearc/oil.nvim",
+        lazy = false,
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            local oil = require "oil"
+            oil.setup {
+                columns = {
+                    "icon",
+                    -- "permissions",
+                    -- "size",
+                    -- "mtime",
+                },
+                float = {
+                    max_width = 80,
+                    max_height = 30,
+                },
+                delete_to_trash = true,
+            }
+
+            local function augroup(name)
+                return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+            end
+
+            vim.api.nvim_create_autocmd({ "VimEnter" }, {
+                group = augroup "oil",
+                pattern = ".",
+                callback = function()
+                    oil.open(oil.get_current_dir())
+                end,
+            })
+        end,
+        keys = {
+            {
+                "<leader>e",
+                function()
+                    if vim.bo.filetype == "oil" then
+                        require("oil").close()
+                    else
+                        require("oil").open_float()
+                    end
+                end,
+                desc = "Toggle Oil",
+            },
+        },
+    },
+}
