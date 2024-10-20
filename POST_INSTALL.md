d4fa77cead66bd0270e255a821b794572319d067
Kevin Ruiz <niveksoft@proton.me>
2024-10-20 00:49:42 -0600

    add post install guides

---
+++ POST_INSTALL.md
@@ -0,0 +1,147 @@
+# Post Install Setup
+
+Setup steps for a new installation of archlinux with archinstall gnome desktop.
+
+## Missing packages
+
+```sh
+sudo pacman -S --needed zsh git less stow neovim wezterm starship 
+```
+
+## Yay
+
+```sh
+sudo pacman -S --needed git base-devel && \
+git clone https://aur.archlinux.org/yay-bin.git && \
+cd yay-bin && \
+makepkg -si && \
+yay -Y --gendb && \
+yay -Y --devel --save
+```
+
+## Zshell
+
+```sh
+chsh -s "$(which zsh)" && \
+yay -S zsh-zim-git
+```
+
+## Fonts
+
+```sh
+sudo pacman -S --needed noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-nerd-fonts-symbols ttf-ubuntu-font-family
+```
+
+## Clipboard
+
+```sh
+sudo pacman -S --needed wl-clipboard && \
+wl-paste --primary --watch wl-copy
+```
+
+## Firewall
+
+```sh
+sudo pacman -S --needed firewalld && \
+sudo systemctl enable --now firewalld.service
+```
+
+## Encryption
+
+If you chose to add encryption, the default iteration time is quite long.
+
+```sh
+sudo cryptsetup luksChangeKey /dev/nvme0n1p2 --iter-time 1000
+```
+
+## Bluetooth
+
+Bluetooth is not enabled by default.
+
+```sh
+sudo systemctl enable bluetooth && \
+sudo systemctl start bluetooth
+```
+
+## Reflector
+
+Make sure to keep your mirrorlist updated.
+
+```sh
+sudo pacman -S --needed reflector
+
+sudo tee /etc/xdg/reflector/reflector.conf > /dev/null << EOF
+# Reflector configuration file for the systemd service.
+#
+# Empty lines and lines beginning with "#" are ignored. All other lines should
+# contain valid reflector command-line arguments. The lines are parsed with
+# Python's shlex modules so standard shell syntax should work. All arguments are
+# collected into a single argument list.
+#
+# See "reflector --help" for details.
+# Recommended Options
+# Set the output path where the mirrorlist will be saved (--save).
+--save /etc/pacman.d/mirrorlist
+# Select the transfer protocol (--protocol).
+--protocol https
+# Select the country (--country).
+# Consult the list of available countries with "reflector --list-countries" and
+# select the countries nearest to you or the ones that you trust. For example:
+--country Canada
+# Use only the most recently synchronized mirrors (--latest).
+--latest 5
+# Sort the mirrors by synchronization time (--sort).
+--sort rate
+EOF
+
+sudo systemctl enable reflector.timer && \
+sudo systemctl start reflector.timer
+```
+
+## Printer
+
+[Guide](https://discovery.endeavouros.com/network/printers/2021/03/#network-printer)
+
+```sh
+sudo pacman -Syu cups cups-pdf
+sudo systemctl enable --now cups.service && \
+sudo systemctl enable --now cups.socket && \
+```
+
+Edit `/etc/nsswitch.conf` and add `mdns_minimal [NOTFOUND=return]` before `resolve`
+
+Note: make sure mdns is enabled in firewalld for `home`
+
+```sh
+sudo systemctl restart NetworkManager
+```
+
+OR add manually if the found printer doesn't work
+
+```sh
+lpadmin -p PRINTER-NAME -E -v "ipp://11.22.33.44/ipp/print" -m everywhere
+```
+
+## Unnecessary GNOME packages
+
+```sh
+sudo pacman -Runs gnome-console epiphany gnome-text-editor
+```
+
+## Gnome Extensions
+
+```sh
+yay -S --needed --noconfirm gnome-shell-extension-blur-my-shell \
+gnome-shell-extension-dash-to-dock \
+gnome-shell-extension-dash-to-dock \
+gnome-shell-extension-just-perfection-desktop \
+gnome-shell-extension-space-bar-git \
+gnome-shell-extension-pop-shell-git \
+```
+
+## Themes
+
+```sh
+yay -S --needed --noconfirm banana-cursor-bin \
+yaru
+```
---
+++ scripts/gnome-preference2.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+
+set -e
+
+write() {
+    dconf write "$1" "$2"
+}
+
+# GNOME Desktop Interface
+write /org/gnome/desktop/interface/accent-color "'blue'"
+write /org/gnome/desktop/interface/clock-format "'12h'"
+write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+write /org/gnome/desktop/interface/cursor-theme "'Banana'"
+write /org/gnome/desktop/interface/enable-hot-corners "false"
+write /org/gnome/desktop/interface/gtk-theme "'Yaru-blue-dark'"
+write /org/gnome/desktop/interface/icon-theme "'Yaru-blue-dark'"
+write /org/gnome/desktop/interface/show-battery-percentage "true"
+
+# Session
+write /org/gnome/desktop/session/idle-delay "uint32 600"
+
+# Fonts
+write /org/gnome/desktop/interface/font-name "'Ubuntu 11'"
+write /org/gnome/desktop/interface/document-font-name "'Ubuntu 11'"
+write /org/gnome/desktop/interface/monospace-font-name "'Ubuntu Mono 10'"
+
+# File Chooser
+write /org/gtk/settings/file-chooser/clock-format "'12h'"
+
+# Date and Time
+write /org/gnome/desktop/datetime/automatic-timezone "true"
+
+# Background
+write /org/gnome/desktop/background/picture-uri "'file:///usr/share/backgrounds/gnome/pixels-d.jxl'"
+write /org/gnome/desktop/background/picture-uri-dark "'file:///usr/share/backgrounds/gnome/pixels-l.jxl'"
+write /org/gnome/desktop/background/primary-color "'#967864'"
+
+# Screensaver
+write /org/gnome/desktop/screensaver/picture-uri "'file:///usr/share/backgrounds/gnome/pixels-l.jxl'"
+write /org/gnome/desktop/screensaver/primary-color "'#967864'"
+
+# Mutter
+write /org/gnome/mutter/overlay-key "'Super_R'"
+write /org/gnome/mutter/dynamic-workspaces "false"
+write /org/gnome/mutter/edge-tiling "false"
+write /org/gnome/mutter/experimental-features "['variable-refresh-rate']"
+write /org/gnome/mutter/workspaces-only-on-primary "true"
+
+# Window Manager Preferences
+write /org/gnome/desktop/wm/preferences/num-workspaces "6"
+
+# Window Manager Keybindings
+write /org/gnome/desktop/wm/keybindings/activate-window-menu "['<Super>Enter']"
+write /org/gnome/desktop/wm/keybindings/begin-move "@as []"
+write /org/gnome/desktop/wm/keybindings/begin-resize "@as []"
+write /org/gnome/desktop/wm/keybindings/close "['<Super>q']"
+write /org/gnome/desktop/wm/keybindings/maximize "@as []"
+write /org/gnome/desktop/wm/keybindings/move-to-workspace-1 "['<Shift><Super>1']"
+write /org/gnome/desktop/wm/keybindings/move-to-workspace-2 "['<Shift><Super>2']"
+write /org/gnome/desktop/wm/keybindings/move-to-workspace-3 "['<Shift><Super>3']"
+write /org/gnome/desktop/wm/keybindings/move-to-workspace-4 "['<Shift><Super>4']"
+write /org/gnome/desktop/wm/keybindings/move-to-workspace-5 "['<Shift><Super>5']"
+write /org/gnome/desktop/wm/keybindings/move-to-workspace-6 "['<Shift><Super>6']"
+write /org/gnome/desktop/wm/keybindings/move-to-workspace-7 "['<Shift><Super>7']"
+write /org/gnome/desktop/wm/keybindings/move-to-workspace-8 "['<Shift><Super>8']"
+write /org/gnome/desktop/wm/keybindings/move-to-workspace-9 "['<Shift><Super>9']"
+write /org/gnome/desktop/wm/keybindings/move-to-workspace-left "@as []"
+write /org/gnome/desktop/wm/keybindings/move-to-workspace-right "@as []"
+write /org/gnome/desktop/wm/keybindings/switch-to-workspace-1 "['<Super>1']"
+write /org/gnome/desktop/wm/keybindings/switch-to-workspace-2 "['<Super>2']"
+write /org/gnome/desktop/wm/keybindings/switch-to-workspace-3 "['<Super>3']"
+write /org/gnome/desktop/wm/keybindings/switch-to-workspace-4 "['<Super>4']"
+write /org/gnome/desktop/wm/keybindings/switch-to-workspace-5 "['<Super>5']"
+write /org/gnome/desktop/wm/keybindings/switch-to-workspace-6 "['<Super>6']"
+write /org/gnome/desktop/wm/keybindings/switch-to-workspace-7 "['<Super>7']"
+write /org/gnome/desktop/wm/keybindings/switch-to-workspace-8 "['<Super>8']"
+write /org/gnome/desktop/wm/keybindings/switch-to-workspace-9 "['<Super>9']"
+write /org/gnome/desktop/wm/keybindings/switch-to-workspace-last "@as []"
+write /org/gnome/desktop/wm/keybindings/switch-to-workspace-left "['<Super>Left']"
+write /org/gnome/desktop/wm/keybindings/switch-to-workspace-right "['<Super>Right']"
+write /org/gnome/desktop/wm/keybindings/toggle-fullscreen "@as []"
+write /org/gnome/desktop/wm/keybindings/toggle-maximized "@as []"
+write /org/gnome/desktop/wm/keybindings/unmaximize "@as []"
+
+# Mutter Wayland Keybindings
+write /org/gnome/mutter/wayland/keybindings/restore-shortcuts "@as []"
+
+# Shell Keybindings
+write /org/gnome/shell/keybindings/focus-active-notification "@as []"
+write /org/gnome/shell/keybindings/toggle-application-view "@as []"
+write /org/gnome/shell/keybindings/toggle-overview "@as []"
+write /org/gnome/shell/keybindings/toggle-quick-settings "@as []"
+
+
+# Power
+write /org/gnome/settings-daemon/plugins/power/ambient-enabled "false"
+
+# Media Keys
+write /org/gnome/settings-daemon/plugins/media-keys/logout "@as []"
+write /org/gnome/settings-daemon/plugins/media-keys/screensaver "@as []"
+
+# Terminal Application
+write /org/gnome/applications/terminal/exec "'wezterm'"
+write /org/gnome/applications/terminal/exec-arg "''"
+
+# Custom Keybindings
+write /org/gnome/settings-daemon/plugins/media-keys/home "['<Super>f']"
+write /org/gnome/settings-daemon/plugins/media-keys/magnifier "@as []"
+write /org/gnome/settings-daemon/plugins/media-keys/magnifier-zoom-in "@as []"
+write /org/gnome/settings-daemon/plugins/media-keys/magnifier-zoom-out "@as []"
+write /org/gnome/settings-daemon/plugins/media-keys/screenreader "@as []"
+write /org/gnome/settings-daemon/plugins/media-keys/www "['<Super>b']"
+
+write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Super>t'"
+write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'wezterm'"
+write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "'Wezterm'"
+write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings "@as ['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+
+# Shell Extensions
+write /org/gnome/shell/disable-user-extensions "false"
+write /org/gnome/shell/disabled-extensions "@as []"
+write /org/gnome/shell/enabled-extensions "['blur-my-shell@aunetx', 'dash-to-dock@micxgx.gmail.com', 'just-perfection-desktop@just-perfection', 'space-bar@luchrioh', 'user-theme@gnome-shell-extensions.gcampax.github.com', 'pop-shell@system76.com']"
+
+# Blur my Shell Extension Settings
+write /org/gnome/shell/extensions/blur-my-shell/overview "false"
+write /org/gnome/shell/extensions/blur-my-shell/panel "false"
+write /org/gnome/shell/extensions/blur-my-shell/dash-to-dock "false"
+
+# Dash to Dock Extension Settings
+write /org/gnome/shell/extensions/dash-to-dock/apply-custom-theme "false"
+write /org/gnome/shell/extensions/dash-to-dock/autohide "true"
+write /org/gnome/shell/extensions/dash-to-dock/custom-theme-shrink "true"
+write /org/gnome/shell/extensions/dash-to-dock/dash-max-icon-size "36"
+write /org/gnome/shell/extensions/dash-to-dock/disable-overview-on-startup "true"
+write /org/gnome/shell/extensions/dash-to-dock/dock-position "'LEFT'"
+write /org/gnome/shell/extensions/dash-to-dock/extend-height "false"
+write /org/gnome/shell/extensions/dash-to-dock/hot-keys "false"
+write /org/gnome/shell/extensions/dash-to-dock/intellihide "false"
+write /org/gnome/shell/extensions/dash-to-dock/show-dock-urgent-notify "false"
+write /org/gnome/shell/extensions/dash-to-dock/show-icons-emblems "false"
+write /org/gnome/shell/extensions/dash-to-dock/show-mount-network "true"
+write /org/gnome/shell/extensions/dash-to-dock/show-show-apps-button "false"
+write /org/gnome/shell/extensions/dash-to-dock/show-windows-preview "false"
+write /org/gnome/shell/extensions/dash-to-dock/workspace-agnostic-urgent-windows "false"
+
+# Space Bar Extension Settings
+write /org/gnome/shell/extensions/space-bar/shortcuts/enable-activate-workspace-shortcuts "true"
+write /org/gnome/shell/extensions/space-bar/shortcuts/enable-move-to-workspace-shortcuts "true"
+
+# Just Perfection Extension Settings
+write /org/gnome/shell/extensions/just-perfection/theme "true"
+write /org/gnome/shell/extensions/just-perfection/workspace-popup "false"
+
+# User Theme Extension Settings
+write /org/gnome/shell/extensions/user-theme/name "'Yaru-blue-dark'"
+
+# Pop Shell Extension Settings
+write /org/gnome/shell/extensions/pop-shell/active-hint "true"
+write /org/gnome/shell/extensions/pop-shell/activate-hint-border-radius "uint32 2"
+write /org/gnome/shell/extensions/pop-shell/activate-launcher "['<Super>d']"
+write /org/gnome/shell/extensions/pop-shell/fullscreen-launcher "true"
+write /org/gnome/shell/extensions/pop-shell/gap-inner "uint32 1"
+write /org/gnome/shell/extensions/pop-shell/gap-outer "uint32 1"
+write /org/gnome/shell/extensions/pop-shell/hint-color-rgba "'rgb(0, 166, 239)'"
+write /org/gnome/shell/extensions/pop-shell/pop-monitor-down "@as []"
+write /org/gnome/shell/extensions/pop-shell/pop-monitor-left "@as []"
+write /org/gnome/shell/extensions/pop-shell/pop-monitor-right "@as []"
+write /org/gnome/shell/extensions/pop-shell/pop-monitor-up "@as []"
+write /org/gnome/shell/extensions/pop-shell/pop-workspace-down "@as []"
+write /org/gnome/shell/extensions/pop-shell/pop-workspace-up "@as []"
+write /org/gnome/shell/extensions/pop-shell/show-title "true"
+write /org/gnome/shell/extensions/pop-shell/tile-by-default "true"
+write /org/gnome/shell/extensions/pop-shell/tile-enter "['<Super>e']"
+
+# Additional Window Manager Keybindings
+write /org/gnome/desktop/wm/keybindings/minimize "@as []"
