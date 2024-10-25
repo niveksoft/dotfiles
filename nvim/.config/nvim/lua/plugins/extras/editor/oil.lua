0048cb720af4d037981638c1cfe1b7b794728ee3
Kevin Ruiz <niveksoft@proton.me>
2024-10-25 15:13:44 -0600

    use opts instead of config for oil

--- nvim/.config/nvim/lua/plugins/extras/editor/oil.lua
+++ nvim/.config/nvim/lua/plugins/extras/editor/oil.lua
@@ -4,21 +4,8 @@ return {
         "stevearc/oil.nvim",
         lazy = false,
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
+        opts = function()
             local oil = require "oil"
-            oil.setup {
-                columns = {
-                    "icon",
-                    -- "permissions",
-                    -- "size",
-                    -- "mtime",
-                },
-                float = {
-                    max_width = 80,
-                    max_height = 30,
-                },
-                delete_to_trash = true,
-            }
 
             local function augroup(name)
                 return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
@@ -31,6 +18,23 @@ return {
                     oil.open(oil.get_current_dir())
                 end,
             })
+
+            return {
+                columns = {
+                    "icon",
+                    "permissions",
+                    -- "size",
+                    -- "mtime",
+                },
+                float = {
+                    max_width = 80,
+                    max_height = 30,
+                },
+                delete_to_trash = true,
+                view_options = {
+                    show_hidden = true,
+                },
+            }
         end,
         keys = {
             {
