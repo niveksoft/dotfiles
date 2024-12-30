c7b302577834a28fb802dab6225561f5570df99b
Kevin Ruiz <niveksoft@proton.me>
2024-12-29 19:45:11 -0700

    add keybind for disabling formatting on current buffer

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,5 +5,15 @@
 local util = require "lazyvim.util"
 
 -- Lazydocker
--- stylua: ignore
-vim.keymap.set("n", "<leader>gd", function() util.terminal({ "lazydocker" }, { esc_esc = false, ctrl_hjkl = false }) end, { desc = "Lazydocker" })
+vim.keymap.set("n", "<leader>gd", function()
+    util.terminal({ "lazydocker" }, { esc_esc = false, ctrl_hjkl = false })
+end, { desc = "Lazydocker" })
+
+-- Formatting
+vim.keymap.set("n", "<leader>co", function()
+    local buffer_format = vim.b.autoformat
+    -- If buffer_format is nil (unset), treat it as true since that's LazyVim's default
+    buffer_format = buffer_format == nil and true or buffer_format
+    vim.b.autoformat = not buffer_format
+    vim.notify("Autoformat " .. (vim.b.autoformat == true and "enabled" or "disabled"), vim.log.levels.INFO)
+end, { desc = "Toggle formatting on current buffer" })
